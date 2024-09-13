a04edba53146978c59a846d23342d7c064294388
Ritika <rkjane333@gmail.com>
2024-09-13 23:43:20 +0530

    removing duplicate elements from array

---
+++ 503_RemoveElement(array).py
@@ -0,0 +1,20 @@
+'''Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+
+Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+
+Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
+Return k.'''
+class Solution(object):
+    def removeElement(self, nums, val):
+        index = 0  
+        for i in range(len(nums)):
+            if nums[i] != val:  
+                nums[index] = nums[i] 
+                index += 1  
+        return index        
+
+nums=[3,2,2,3]       
+sol=Solution()
+res=(sol.removeElement(nums,3))     
+print(nums)    
+print(res)
