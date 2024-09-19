8eb8662872bc8ff54e877778c1dd931e7b3fed73
Ritika <rkjane333@gmail.com>
2024-09-19 23:38:48 +0530

    fibonacci number using recursion

---
+++ 600_Fibonacci_Number(recursion).py
@@ -0,0 +1,23 @@
+'''The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, 
+such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+
+F(0) = 0, F(1) = 1
+F(n) = F(n - 1) + F(n - 2), for n > 1.
+Given n, calculate F(n).'''
+
+
+class Solution(object):
+    def fib(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n==0:
+            return 0
+        if n==1:
+            return 1
+        if n>1:
+            return self.fib(n-1) + self.fib(n-2)        
+n=2
+sol=Solution()
+print(sol.fib(n))  
