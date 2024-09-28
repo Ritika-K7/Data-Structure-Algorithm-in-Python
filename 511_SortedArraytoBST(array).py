3f5afe5bca375723edc1cc128c5e0988f3990fe0
Ritika <rkjane333@gmail.com>
2024-09-28 22:54:35 +0530

    sorted array to binary search tree

---
+++ 511_SortedArraytoBST(array).py
@@ -0,0 +1,29 @@
+'''Given an integer array nums where the elements are sorted in ascending order, convert it to a 
+height-balanced binary search tree. '''
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+        
+class Solution(object):
+    def sortedArrayToBST(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: TreeNode
+        """
+        if not nums:
+            return None
+
+        mid=len(nums)//2
+        root=TreeNode(nums[mid])
+        root.left=self.sortedArrayToBST(nums[:mid])
+        root.right=self.sortedArrayToBST(nums[mid+1:])
+        return root
+nums=[-10,-3,0,5,9]
+sol=Solution()
+print(sol.sortedArrayToBST(nums))            
+        
